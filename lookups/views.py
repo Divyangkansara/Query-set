e427f879113f9d910379ba54a060fbeb420613c1
Divyang <divyang.kansara@technostacks.com>
2024-02-15 18:23:53 +0530

    queries

--- lookups/views.py
+++ lookups/views.py
@@ -7,11 +7,15 @@ def home(request):
     # # student_data = Student.objects.filter(name__iexact='Rahul')
     # student_data = Student.objects.filter(name__icontains='k')
     # student_data = Student.objects.filter(id__in = [1,5,7])
-    student_data = Student.objects.filter(marks__in = [80])
+    #student_data = Student.objects.filter(marks__in = [80])
     # student_data = Student.objects.filter(marks__gt = 70)    #lt
     # student_data = Student.objects.filter(marks__gte = 70)   #lte
     # student_data = Student.objects.filter(name__startswith = 'R')
-    # student_data = Student.objects.filter(city__startswith = 'b')     
+    student_data = Student.objects.filter(city__startswith = 'b') | Student.objects.filter(city__startswith = 'a')
+    # student_data = Student.objects.filter(id__range = (1,6))
+    
+
+         
     print("Return:", student_data)
     print()
     print("SQL Query:", student_data.query)
